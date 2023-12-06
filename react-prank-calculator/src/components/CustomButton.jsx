497dda99de79ecddfe944990ec5f5f1c3f6943c9
Rajan Himang <rajanhimang@gmail.com>
2023-12-06 12:01:03 +1100

    my app

---
+++ react-prank-calculator/src/components/CustomButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CustomButton({ label, cls, clickHandler }) {
+  return (
+    <div className={cls} onClick={() => clickHandler(label)}>
+      {label}
+    </div>
+  );
+}
+
+export default CustomButton;
